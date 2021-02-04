a4ed80b42e1762d2f1452dccad0fc93db625cd00
Misbah Ahmed <72851859+misbah-99@users.noreply.github.com>
2021-02-04 09:32:23 +0600

    Leaderboard

---
+++ leaderboard.py
@@ -0,0 +1,9 @@
+"""
+A program to display numbers from 1 to 9 each on a separate line following a dot.
+
+Heads Up: Copying without credit is restricted.
+
+"""
+
+for i in range(1, 10):
+    print(f"{i}.")
